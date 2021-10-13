f9ec76c11e6d5ca1b520cb5684f0729f05bf9f0b
sungj <787413807@qq.com>
2021-10-13 09:36:44 +0800

    需求修改

--- src/views/pages/systemSet/thresholdParameters.vue
+++ src/views/pages/systemSet/thresholdParameters.vue
@@ -5,17 +5,29 @@
                 <el-input :disabled="readOnly" v-model="form.id" ></el-input>
             </el-form-item>
             <div class="clear"></div> -->
-            <el-form-item label="置信度"  :style="itemStyle">
+            <!-- <el-form-item label="置信度"  :style="itemStyle">
                 <el-input :disabled="readOnly" v-model="form.confidence" ></el-input>
             </el-form-item>
             <div class="clear"></div>
             <el-form-item label="振动次数" :style="itemStyle" >
                 <el-input :disabled="readOnly" v-model="form.shake_count" ></el-input>
             </el-form-item>
+            <div class="clear"></div> -->
+
+            <el-form-item label="告警等级"  :style="itemStyle">
+                <el-select :disabled="readOnly" v-model="form.alarm_level" placeholder="请选择告警等级">
+                    <el-option label="无告警" value="0"></el-option>
+                    <el-option label="预告警（监测到有振动，但是尚不告警）" value="1"></el-option>
+                    <el-option label="弱告警" value="2"></el-option>
+                    <el-option label="中告警" value="3"></el-option>
+                    <el-option label="强告警" value="4"></el-option>
+                </el-select>
+            </el-form-item>
             <div class="clear"></div>
+
         </el-form>    
         <div class="clear"></div>
-        <!-- <div style="margin-left:150px">
+        <div style="margin-left:150px">
             <el-button v-show="readOnly" type="primary" style="width:150px;margin-bottom:30px;" @click="readOnly=false">
                 修 改
             </el-button>
@@ -25,7 +37,7 @@
             <el-button v-show="!readOnly" style="width:150px;margin-bottom:30px;" @click="readOnly=true">
                 放 弃
             </el-button>
-        </div> -->
+        </div>
     </div>
 </template>
 
@@ -38,8 +50,9 @@ export default {
             readOnly:true,
             form:{
                 id:'',
-                confidence:'',
-                shake_count:'',
+                // confidence:'',
+                // shake_count:'',
+                alarm_level:''
             },
         }
     },
